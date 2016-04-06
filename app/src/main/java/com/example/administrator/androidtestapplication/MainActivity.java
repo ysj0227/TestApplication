02284ec17fe14664a67d84640e616c8aaecb1f16
ysj0227 <1976290043@qq.com>
2016-04-06 17:20:34 +0800

    new fix

--- app/src/main/java/com/example/administrator/androidtestapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/androidtestapplication/MainActivity.java
@@ -29,9 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        Log.i("TAG","---111---");
-        Log.i("TAG","---aaa---");
+        Log.i("TAG","---aaa22---");
         Log.i("TAG","---ccc---");
         Log.i("TAG","---ddd---");
         Log.i("TAG","---ggg---");
