6a07adc5e8fd6ecdcb4a73f1a8bb63b2659253a9
ysj0227 <1976290043@qq.com>
2016-04-06 11:00:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/androidtestapplication/aa.java
@@ -0,0 +1,15 @@
+package com.example.administrator.androidtestapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class aa extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
